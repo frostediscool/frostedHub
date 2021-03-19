1fa3100d635b4e79ef4c7186764064c4a7e6072f
frosted <zaheermiah512@gmail.com>
2021-03-19 19:46:05 +0000

    Initial Commit

---
+++ ScriptHub/Loader.lua
@@ -0,0 +1,6 @@
+local Utilities = loadstring(game:HttpGet("https://raw.githubusercontent.com/derekcool38/Releases/main/Functions/Utilities.lua"))()
+if game.PlaceId == 3956818381 then -- Ninja Legends
+    Utilities:Load("https://raw.githubusercontent.com/frostediscool/bruhHub/master/NinjaLegends/NinjaLegendsHub.lua")
+else
+    game:GetService("Players").LocalPlayer:Kick("[frostedHub] Game not Supported")
+end
---
+++ ScriptHub/Utilities.lua
@@ -0,0 +1,5 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Welcome to frostedHub!";
+    Text = "Enjoy.";
+    Duration = "2";
+})
