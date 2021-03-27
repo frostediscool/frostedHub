904307451762887970175301ba9d12c99b175507
frosted <zaheermiah512@gmail.com>
2021-03-27 13:01:19 +0000

    Initial Commit

--- ScriptHub/Loader.lua
+++ ScriptHub/Loader.lua
@@ -1,8 +1,8 @@
-local Utilities = loadstring(game:HttpGet("https://raw.githubusercontent.com/frostediscool/bruhHub/master/ScriptHub/Utilities.lua"))()
+local Utilities = loadstring(game:HttpGet("https://raw.githubusercontent.com/frostediscool/frostedHub/master/ScriptHub/Utilities.lua"))()
 if game.PlaceId == 3956818381 then -- Ninja Legends
-    Utilities:Load("https://raw.githubusercontent.com/frostediscool/bruhHub/master/NinjaLegends/NinjaLegendsHub.lua")
+    Utilities:Load("https://raw.githubusercontent.com/frostediscool/frostedHub/master/NinjaLegends/NinjaLegendsHub.lua")
 elseif game.PlaceId == 3823781113 then -- Saber Simulator
-    Utilities:Load("")
+    Utilities:Load("https://github.com/frostediscool/frostedHub/blob/master/SaberSimulator/SaberSimulatorHub.lua")
 else
     game:GetService("Players").LocalPlayer:Kick("[frostedHub] | Game not Supported")
 end
